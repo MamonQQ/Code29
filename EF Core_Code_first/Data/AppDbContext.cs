d9cae782dd5510807b2a37e941e8e071313d6e3c
MamonQQ <165375761+MamonQQ@users.noreply.github.com>
2024-04-03 14:11:39 +0700

    test

---
+++ EF Core_Code_first/Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EF_Core_Code_first.Models;
+
+namespace EF_Core_Code_first.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+		}
+
+		public DbSet<Students> Students { get; set; }
+		public DbSet<Courses> Courses { get; set; }
+		public DbSet<StudentCourses> StudentCourses { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.Entity<StudentCourses>()
+				.HasKey(sc => new
+				{
+					sc.StudentId,
+					sc.CoursesId
+				});
+		}
+	}
+}
